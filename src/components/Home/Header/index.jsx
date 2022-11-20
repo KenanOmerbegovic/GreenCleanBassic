4a2038f60730d13e755007b34c746bf200361d9c
KenanOmerbegovic <71610207+KenanOmerbegovic@users.noreply.github.com>
2022-11-20 08:04:26 +0100

    Add files via upload

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+      <img src="/assets/images/logo2.png" height="140rem" alt="" />
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,87 @@
+export const blogList = [
+  {
+    id: 1,
+    title: 'City park ',
+    category: 'development',
+    subCategory: ['camping', 'forest', 'small amount'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/designer-1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Camping spot',
+    category: 'travel',
+    subCategory: ['vacation', 'water polution', 'sight seeing'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Cleaner',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/f67396fc3cfce63a28e07ebb35d974ac.jpg',
+  },
+  {
+    id: 3,
+    title: 'Forest trail',
+    category: 'Forest trail',
+    subCategory: ['forest', 'Dense', 'trail'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'User2',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/fQwuyKJ9qxjSbr6REcgtmW-1200-80.jpg',
+  },
+  {
+    id: 4,
+    title: 'River side',
+    category: 'River side',
+    subCategory: ['adrenaline', 'River side', 'Trail'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Volunteer',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/graphic-design-trends.jpg',
+  },
+  {
+    id: 5,
+    title: 'Jogging trail',
+    category: 'Trail',
+    subCategory: ['near road', 'food', 'Trail'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/make-it-personal.jpg',
+  },
+  {
+    id: 6,
+    title: 'Beyond the Beach',
+    category: 'travel',
+    subCategory: ['beaches', 'sea', 'water polution'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover:
+      '/assets/images/Purple-Combination-colors-graphic-design-predictions-1024x576-1024x576.jpg',
+  },
+  {
+    id: 7,
+    title: 'Polluted lake',
+    category: 'lake',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Cleaner2',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/Synthwave-Postmodern.jpg',
+  },
+];
